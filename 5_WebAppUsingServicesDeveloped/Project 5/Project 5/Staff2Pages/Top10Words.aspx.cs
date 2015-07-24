cf7a65121a305055af65ea4e80e102d9ad8034b1
jvutukur <jvutukur@asu.edu>
2015-07-23 17:45:23 -0700

    Added files

---
+++ 5_WebAppUsingServicesDeveloped/Project 5/Project 5/Staff2Pages/Top10Words.aspx.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Project_5.Staff2Pages
+{
+    public partial class Top10Words : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Unnamed_LoggingOut(object sender, LoginCancelEventArgs e)
+        {
+            Context.GetOwinContext().Authentication.SignOut();
+            Session["type"] = "";
+        }
+
+        protected void FindTop10WordsButton_Click(object sender, EventArgs e)
+        {
+            RequiredServicesClient.Service1Client top10WordsServiceClient = new RequiredServicesClient.Service1Client();
+            String url = Url_TextBox.Text;
+            String[] top10Words = top10WordsServiceClient.Top10Words(url);
+            String msg = "";
+            try
+            {
+                foreach (String word in top10Words)
+                {
+                    msg = msg + word + "\n";
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+            if (msg == "")
+            {
+                Top10WordsOutputTextBox.Enabled = true;
+                Top10WordsOutputTextBox.Text = "Incorrect URL Please try again";
+                Top10WordsOutputTextBox.Enabled = false;
+            }
+            else
+            {
+                Top10WordsOutputTextBox.Enabled = true;
+                Top10WordsOutputTextBox.Text = msg;
+                Top10WordsOutputTextBox.Enabled = false;
+            }
+
+        }
+    }
+}
---
+++ 5_WebAppUsingServicesDeveloped/Project 5/Project 5/StaffLogin.aspx.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Xml;
+using System.Web.Security;
+using System.Text.RegularExpressions;
+
+namespace Project_5
+{
+    public partial class StaffLogin : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void loginbutton_Click(object sender, EventArgs e)
+        {
+            DecryptionService.Service1 dec = new DecryptionService.Service1();
+
+            String username=usernametb.Text;
+            String pwd=pwdtb.Text;
+
+            String pathofxmlFile=  HttpContext.Current.Server.MapPath(@"~\") + "\\" + "App_Data" + "\\" + "staffCredentials.xml";
+            //Request.PhysicalApplicationPath
+            //HttpRuntime.AppDomainAppPath
+
+            XmlDocument xd = new XmlDocument();
+            xd.Load(pathofxmlFile);
+            
+            XmlNode node=xd.DocumentElement;
+            XmlNodeList children=node.ChildNodes;
+            int flag=0;
+            
+            
+            foreach(XmlNode child in children)
+            {
+                if(child.FirstChild.InnerText.Equals(username))
+                {
+                    String encpwd=child.SelectSingleNode("password").InnerText;
+                     
+                    String decpwd = dec.decryption(encpwd);
+                    decpwd = Regex.Replace(decpwd, "\0", "");
+                    if (decpwd.Equals(pwd))
+                    {
+                        flag++;
+                        Session["type"] = "staff";
+                        break;
+                    }
+                }
+            }
+            if(flag==0)
+            {
+                FailureText.Text="Invalid credentails. Please Try again";
+                ErrorMessage.Visible = true;
+            }
+            if(flag!=0)
+            {
+                Session["name"] = username;
+                FormsAuthentication.RedirectFromLoginPage(username, true);
+                Response.Redirect("~/StaffPages/EncDec");
+            }
+        }
+    }
+}
